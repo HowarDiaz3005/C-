fef4533356c78d64809821583d1c0648fc6c69af
Howard Diaz <141751595+HowarDiaz3005@users.noreply.github.com>
2024-01-29 16:28:38 -0600

    Add files via upload

---
+++ SESION-29-01-24/Program.cs
@@ -0,0 +1,23 @@
+﻿
+    int nro;
+    Console.Title = "BUSQUEDA DE NUMEROS PARES";
+    Console.Write("/\nIngrese un numero");
+    nro = int.Parse(Console.ReadLine());    
+    if (nro < 0)
+    {
+        Console.Write("\n\tNumero Negativo..... Ingrese un numero positivo");
+    }
+    else if (nro % 2 == 0)
+    {
+        Console.Write("\n\tEl numero (" + nro + ") es par.");
+        if (nro >= 10 && nro <= 100)
+        {
+            Console.Write("\n\tEl numero (" + nro + ") se encuentra en el rango[10 - 100]");
+        }
+    }
+    else
+    {
+        Console.Write("El numero ingresado es impar..");
+
+    }
+    Console.ReadKey();  
