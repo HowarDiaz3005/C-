bdac340661df23e05786f44f4179646a6e0fccc6
Howard Diaz <141751595+HowarDiaz3005@users.noreply.github.com>
2024-01-30 14:50:59 -0600

    Add files via upload

---
+++ SESION-30-01-24/Program3.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("/\n=================="); 
+Console.Write("/\nIngrese su nombre:");
+Console.Write("/\n==================");
+String name = Console.ReadLine();
+Console.Write("/\n==================");
+Console.Write("/\nIngrese su edad:");
+Console.Write("/\n==================");
+String ed = Console.ReadLine();
+Console.Write("/\n==================");
+Console.Write("/\nIngrese su sexo ( Hombre = H | Mujer = F):");
+Console.Write("/\n==============--====");
+String sex = Console.ReadLine();
+ //======================
+ // Formulacion de datos.
+ //======================
+ if (sex == "M" || sex == "m")
+{
+    Console.WriteLine("\nEl Sr. {0} ha sido registrado ",name); // v
+}
+ else
+{
+    Console.WriteLine("La Sr. {0} ha sido registrada", name); // m
+}
+ Console.WriteLine("\n\n");
+Console.WriteLine("\n------>Terminado");
+Console.ReadKey();
