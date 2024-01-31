2db5833f95bf4fe7db84013e032914c82f336d7a
Howard Diaz <141751595+HowarDiaz3005@users.noreply.github.com>
2024-01-31 14:57:14 -0600

    Add files via upload

---
+++ SESION-31-01-24/SESION-31-01-24.cs
@@ -0,0 +1,21 @@
+﻿//==================================================
+//Inicializando variables....
+Double Rad;
+Double Peri, A, Vol1;
+Double pi = 3.1416;
+//==================================================
+//==================================================
+Console.WriteLine("Ingrese el radio");
+Rad = Double.Parse(Console.ReadLine());
+//==================================================
+//==================================================
+Peri = (2 * pi * Rad);
+A = (pi * Rad * Rad);
+Vol1 = (4 * pi * Rad * Rad * Rad / 3);
+//==================================================
+//==================================================
+Console.WriteLine("El perimetro de la circunferencia :" + Peri);
+Console.WriteLine("El area del circulo :" + A);
+Console.WriteLine("El volumen de la esfera es :" + Vol1);
+//==================================================
+//==================================================
